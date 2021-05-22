a89ffa334196120d0fdd413f8fd21951c4075064
RejzovicEmir <70068774+RejzovicEmir@users.noreply.github.com>
2021-05-22 14:56:08 +0200

    Add files via upload

---
+++ javaScript.js
@@ -0,0 +1,12 @@
+/* Variables DOM */
+
+const buttonPlay = document.getElementById('buttonPlay');
+const buttonReset = document.getElementById('buttonReset');
+const timeLeftDOM = document.getElementById('timeLeft');
+const labelSessionBreak = document.getElementById('labelSessionBreak');
+const sessionLengthDOM = document.getElementById('sessionLength');
+const breakLengthDOM = document.getElementById('breakLength');
+const sessionDecrement = document.getElementById('sessionDecrement');
+const sessionIncrement = document.getElementById('sessionIncrement');
+const breakDecrement = document.getElementById('breakDecrement');
+const breakIncrement = document.getElementById('breakIncrement');
